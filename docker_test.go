4d01ad3cea798e741fca479675283c7c111fb20e
yorito <orito.yoshiyuki@gmail.com>
2016-07-11 11:36:29 +0900

    Fix failing to obtaing ip when docker machine running on.

--- docker_test.go
+++ docker_test.go
@@ -6,6 +6,9 @@ import (
 	"testing"
 	"time"
 
+	"os"
+	"regexp"
+
 	"github.com/stretchr/testify/require"
 )
 
@@ -34,11 +37,18 @@ func TestRun(t *testing.T) {
 	con := Run("redis")
 	defer con.Close()
 
-	require.Equal(t, "127.0.0.1", con.Host())
+	ip := "127.0.0.1"
+	// for docker-machine
+	if host := os.Getenv("DOCKER_HOST"); host != "" {
+		ipRegex := regexp.MustCompile(`\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}`)
+		ip = ipRegex.FindString(host)
+	}
+
+	require.Equal(t, ip, con.Host())
 
 	port := con.ports[6379]
 
-	require.Equal(t, net.JoinHostPort("127.0.0.1", strconv.Itoa(port)), con.Addr(6379))
+	require.Equal(t, net.JoinHostPort(ip, strconv.Itoa(port)), con.Addr(6379))
 	require.Equal(t, port, con.Port(6379))
 
 }
@@ -61,4 +71,16 @@ func TestWaitHTTP(t *testing.T) {
 	p := con.WaitHTTP(80, "/", 1*time.Second)
 	require.NotZero(t, p)
 
+}
+
+func TestParseIp(t *testing.T) {
+	c := Container{}
+
+	c.parseIp("127.0.0.1")
+	require.NotNil(t, c.host)
+	require.Equal(t, "127.0.0.1", c.host)
+
+	c.parseIp("tcp://192.168.99.100:2376")
+	require.NotNil(t, c.host)
+	require.Equal(t, "192.168.99.100", c.host)
 }
